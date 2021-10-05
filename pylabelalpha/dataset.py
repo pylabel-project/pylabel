de0a5efafd240be0aa527cf52bd1739ac420d2ac
alexheat <aheaton@gmail.com>
2021-10-04 20:26:38 -0700

    Updated

--- pylabelalpha/dataset.py
+++ pylabelalpha/dataset.py
@@ -1,11 +1,13 @@
 from pylabelalpha.analyze import Analyze
 from pylabelalpha.exporter import Export
+from pylabelalpha.splitter import StratifiedGroupShuffleSplit2 
 
 class Dataset:
     def __init__(self, df):
         self.df = df
         self.name = "dataset"
-        self.analyze = Analyze(self.df)   
+        self.analyze = Analyze(self.df) 
+        self.StratifiedGroupShuffleSplit2 = StratifiedGroupShuffleSplit2
         #self.export = Export(self.df)
 
     
