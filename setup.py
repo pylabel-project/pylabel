e3969f5df2c61ecc47024954847372c373fd7dc6
alexheat <aheaton@gmail.com>
2021-10-12 15:48:09 -0700

    Change package name to pylabelalphatest

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pylabelalpha_test22',
+    name='pylabelalphatest',
     packages=['pylabelalpha'],
-    version='0.1.5',
+    version='0.1.1',
     description='My first Python library',
     author='Me',
     license='MIT',
