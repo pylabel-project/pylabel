a0d46d39466f51e549eedee65c040f6a9665d19e
alexheat <aheaton@gmail.com>
2021-10-18 08:07:58 -0700

    Clean up before move to new folder

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import find_packages, setup
 setup(
     name='pylabelalphatest',
     packages=['pylabelalpha'],
-    version='0.1.3',
+    version='0.1.4  ',
     description='My first Python library',
     author='Me',
     license='MIT',
-    install_requires=['pandas'],
+    install_requires=['pandas','bbox_visualizer'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
