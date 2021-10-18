bc49a7714dddd7971b301fe4d19eb69645ab944c
alexheat <aheaton@gmail.com>
2021-10-18 15:32:05 -0700

    Fixed coco

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pylabelalphatest',
     packages=['pylabelalpha'],
-    version='0.1.4  ',
+    version='0.1.5',
     description='My first Python library',
     author='Me',
     license='MIT',
