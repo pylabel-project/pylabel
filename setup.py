7e8cf68e38752a8cdad2cda3687d7412acb9ca00
Alex Heaton <aheaton@gmail.com>
2021-10-24 15:04:08 -0700

    Fixed sample notebooks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='pylabel',
     packages=['pylabel'],
-    version='0.1.1',
+    version='0.1.2',
     description='Transform, analyze, and visualize computer vision annotations.',
     author='PyLabel Project',
     license='MIT',
