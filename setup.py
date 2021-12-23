94e14a205d5a60d0950bdabd63cbf806681e1e34
Alex Heaton <aheaton@gmail.com>
2021-12-23 15:35:00 -0800

    black formatting

--- setup.py
+++ setup.py
@@ -5,14 +5,22 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='pylabel',
-    packages=['pylabel'],
-    version='0.1.20',
-    description='Transform, analyze, and visualize computer vision annotations.',
+    name="pylabel",
+    packages=["pylabel"],
+    version="0.1.22",
+    description="Transform, analyze, and visualize computer vision annotations.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url = 'https://github.com/pylabel-project/pylabel',
-    author='PyLabel Project',
-    license='MIT',
-    install_requires=['pandas','bbox_visualizer','matplotlib','opencv-python','scikit-learn','jupyter_bbox_widget','pyyaml']
+    url="https://github.com/pylabel-project/pylabel",
+    author="PyLabel Project",
+    license="MIT",
+    install_requires=[
+        "pandas",
+        "bbox_visualizer",
+        "matplotlib",
+        "opencv-python",
+        "scikit-learn",
+        "jupyter_bbox_widget",
+        "pyyaml",
+    ],
 )
