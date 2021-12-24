aaf92fd268bdfc99d4e0bce31720c251fce649bb
Alex Heaton <aheaton@gmail.com>
2021-12-24 08:36:37 -0800

    Publish tests

--- tests/README.md
+++ tests/README.md
@@ -2,5 +2,5 @@ Commands to run tests pytest
 
 ```
 pytest
-pytest --nbmake  "dataset_splitting.ipynb" "coco2voc.ipynb" "yolo2coco.ipynb"
+pytest --nbmake  "dataset_splitting.ipynb" "coco2voc.ipynb" "yolo2coco.ipynb" "yolo2voc.ipynb"
 ```
--- tests/__init__.py
+++ tests/__init__.py
@@ -6,16 +6,16 @@ import zipfile
 #Download sample coco dataset 
 os.makedirs("data", exist_ok=True)
 #Download sample dataset
-os.system("wget 'https://github.com/pylabelalpha/notebook/blob/main/BCCD_coco.zip?raw=true' -O data/BCCD_coco.zip")
+os.system("wget 'https://github.com/pylabel-project/datasets_models/raw/main/BCCD/BCCD_coco.zip' -O data/BCCD_coco.zip")
 with zipfile.ZipFile("data/BCCD_coco.zip", 'r') as zip_ref:
     zip_ref.extractall("data")
 
 #Download sample yolo dataset 
-os.makedirs("data", exist_ok=True)
 os.system("wget 'https://github.com/ultralytics/yolov5/releases/download/v1.0/coco128.zip' -O data/coco128.zip")
 with zipfile.ZipFile("data/coco128.zip", 'r') as zip_ref:
    zip_ref.extractall("data")
 
 #Download sample yolo dataset 
+os.system("rm -rf samples")
 os.system("git clone https://github.com/pylabel-project/samples.git")
 
