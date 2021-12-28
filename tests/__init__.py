acdd9dd27dd96bee59b6ee92f4f51bdfe1be1c1e
Alex Heaton <aheaton@gmail.com>
2021-12-28 11:31:09 -0800

    Download sample notebooks so they can nbmake

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,21 +1,25 @@
-import os 
+import os
 import zipfile
-    
-#Download the datasets during the setup phase so they are available when the tests are run
 
-#Download sample coco dataset 
+# Download the datasets during the setup phase so they are available when the tests are run
+
+# Download sample coco dataset
 os.makedirs("data", exist_ok=True)
-#Download sample dataset
-os.system("wget 'https://github.com/pylabel-project/datasets_models/raw/main/BCCD/BCCD_coco.zip' -O data/BCCD_coco.zip")
-with zipfile.ZipFile("data/BCCD_coco.zip", 'r') as zip_ref:
+# Download sample dataset
+os.system(
+    "wget 'https://github.com/pylabel-project/datasets_models/raw/main/BCCD/BCCD_coco.zip' -O data/BCCD_coco.zip"
+)
+with zipfile.ZipFile("data/BCCD_coco.zip", "r") as zip_ref:
     zip_ref.extractall("data")
 
-#Download sample yolo dataset 
-os.system("wget 'https://github.com/ultralytics/yolov5/releases/download/v1.0/coco128.zip' -O data/coco128.zip")
-with zipfile.ZipFile("data/coco128.zip", 'r') as zip_ref:
-   zip_ref.extractall("data")
+# Download sample yolo dataset
+os.system(
+    "wget 'https://github.com/ultralytics/yolov5/releases/download/v1.0/coco128.zip' -O data/coco128.zip"
+)
+with zipfile.ZipFile("data/coco128.zip", "r") as zip_ref:
+    zip_ref.extractall("data")
 
-#Download sample yolo dataset 
+# Download sample notebooks so they can be tested using nbmake
 os.system("rm -rf samples")
 os.system("git clone https://github.com/pylabel-project/samples.git")
-
+os.system("mv samples/*.ipynb ..")
