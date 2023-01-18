53202826414c71f962ad64faca2dad7173868408
Nokrux <julien.deslandes@protonmail.com>
2023-01-18 15:33:41 +0100

    Add yolov7 export

--- pylabel/exporter.py
+++ pylabel/exporter.py
@@ -665,6 +665,282 @@ class Export:
                 output_file_paths = [path_dict["yaml_path"]] + output_file_paths
 
         return output_file_paths
+    
+    def ExportToYoloV7(
+        self,
+        output_path="training/labels",
+        yaml_file="dataset.yaml",
+        copy_images=False,
+        use_splits=False,
+        cat_id_index=None,
+        segmentation=False,
+    ):
+        """Writes annotation files to disk in YOLOv5 format and returns the paths to files.
+
+        Args:
+
+            output_path (str):
+                This is where the annotation files will be written.
+                If not-specified then the path will be derived from the .path_to_annotations and
+                .name properties of the dataset object. If you are exporting images to train a model, the recommended path
+                to use is 'training/labels'.
+            yaml_file (str):
+                If a file name (string) is provided, a YOLOv5 YAML file will be created with entries for the files
+                and classes in this dataset. It will be created in the parent of the output_path directory.
+                The recommended name for the YAML file is 'dataset.yaml'.
+            copy_images (boolean):
+                If True, then the annotated images will be copied to a directory next to the labels directory into
+                a directory named 'images'. This will prepare your labels and images to be used as inputs to
+                train a YOLOv5 model.
+            use_splits (boolean):
+                If True, then the images and annotations will be moved into directories based on the values in the split column.
+                For example, if a row has the value split = "train" then the annotations for that row will be moved to directory
+                /train. If a YAML file is specificied then the YAML file will use the splits to specify the folders user for the
+                train, val, and test datasets.
+            cat_id_index (int):
+                Reindex the cat_id values so that that they start from an int (usually 0 or 1) and
+                then increment the cat_ids to index + number of categories continuously.
+                It's useful if the cat_ids are not continuous in the original dataset.
+                Yolo requires the set of annotations to start at 0 when training a model.
+            segmentation (boolean):
+                If true, then segmentation annotations will be exported instead of bounding box annotations.
+                If there are no segmentation annotations, then no annotations will be empty.
+
+        Returns:
+            A list with 1 or more paths (strings) to annotations files. If a YAML file is created
+            then the first item in the list will be the path to the YAML file.
+
+        Examples:
+            >>> dataset.export.ExportToYoloV5(output_path='training/labels',
+            >>>     yaml_file='dataset.yaml', cat_id_index=0)
+            ['training/dataset.yaml', 'training/labels/frame_0002.txt', ...]
+
+        """
+        ds = self.dataset
+
+        # Inspired by https://github.com/aws-samples/groundtruth-object-detection/blob/master/create_annot.py
+        yolo_dataset = ds.df.copy(deep=True)
+        # Convert nan values in the split column from nan to '' because those are easier to work with with when building paths
+        yolo_dataset.split = yolo_dataset.split.fillna("")
+
+        # Create all of the paths that will be used to manage the files in this dataset
+        path_dict = {}
+
+        # The output path is the main path that will be used to create the other relative paths
+        path = PurePath(output_path)
+        path_dict["label_path"] = output_path
+        # The /images directory should be next to the /labels directory
+        path_dict["image_path"] = str(PurePath(path.parent, "images"))
+        # The root directory is in parent of the /labels and /images directories
+        path_dict["root_path"] = str(PurePath(path.parent))
+        # The YAML file should be in root directory
+        path_dict["yaml_path"] = str(PurePath(path_dict["root_path"], yaml_file))
+        # The root directory will usually be next to the yolov5 directory.
+        # Specify the relative path
+        path_dict["root_path_from_yolo_dir"] = str(PurePath("../"))
+        # If these default values to not match the users environment then they can manually edit the YAML file
+
+        if copy_images and not use_splits:
+            # Create the folder that the images will be copied to
+            Path(path_dict["image_path"]).mkdir(parents=True, exist_ok=True)
+
+        # Drop rows that are not annotated
+        # Note, having zero annotates can still be considered annotated
+        # in cases when are no objects in the image thats should be indentified
+        yolo_dataset = yolo_dataset.loc[yolo_dataset["annotated"] == 1]
+
+        # yolo_dataset["cat_id"] = (
+        #     yolo_dataset["cat_id"].astype("float").astype(pd.Int32Dtype())
+        # )
+
+        yolo_dataset.cat_id = yolo_dataset.cat_id.replace(r"^\s*$", np.nan, regex=True)
+
+        pd.to_numeric(yolo_dataset["cat_id"])
+
+        if cat_id_index != None:
+            assert isinstance(cat_id_index, int), "cat_id_index must be an int."
+            _ReindexCatIds(yolo_dataset, cat_id_index)
+
+        # Convert empty bbox coordinates to nan to avoid math errors
+        # If an image has no annotations then an empty label file will be created
+        yolo_dataset.ann_bbox_xmin = yolo_dataset.ann_bbox_xmin.replace(
+            r"^\s*$", np.nan, regex=True
+        )
+        yolo_dataset.ann_bbox_ymin = yolo_dataset.ann_bbox_ymin.replace(
+            r"^\s*$", np.nan, regex=True
+        )
+        yolo_dataset.ann_bbox_width = yolo_dataset.ann_bbox_width.replace(
+            r"^\s*$", np.nan, regex=True
+        )
+        yolo_dataset.ann_bbox_height = yolo_dataset.ann_bbox_height.replace(
+            r"^\s*$", np.nan, regex=True
+        )
+
+        # If segmentation = False then export bounding boxes
+        if segmentation == False:
+
+            yolo_dataset["center_x_scaled"] = (
+                yolo_dataset["ann_bbox_xmin"] + (yolo_dataset["ann_bbox_width"] * 0.5)
+            ) / yolo_dataset["img_width"]
+            yolo_dataset["center_y_scaled"] = (
+                yolo_dataset["ann_bbox_ymin"] + (yolo_dataset["ann_bbox_height"] * 0.5)
+            ) / yolo_dataset["img_height"]
+            yolo_dataset["width_scaled"] = (
+                yolo_dataset["ann_bbox_width"] / yolo_dataset["img_width"]
+            )
+            yolo_dataset["height_scaled"] = (
+                yolo_dataset["ann_bbox_height"] / yolo_dataset["img_height"]
+            )
+
+        # Create folders to store annotations
+        if output_path == None:
+            dest_folder = PurePath(
+                ds.path_to_annotations, yolo_dataset.iloc[0].img_folder
+            )
+        else:
+            dest_folder = output_path
+        if not use_splits:
+            os.makedirs(dest_folder, exist_ok=True)
+
+        unique_images = yolo_dataset["img_filename"].unique()
+        output_file_paths = []
+
+        for img_filename in unique_images:
+            df_single_img_annots = yolo_dataset.loc[
+                yolo_dataset.img_filename == img_filename
+            ]
+
+            basename, _ = os.path.splitext(img_filename)
+            annot_txt_file = basename + ".txt"
+            # Use the value of the split collumn to create a directory
+            # The values should be train, val, test or ''
+            if use_splits:
+                split_dir = df_single_img_annots.iloc[0].split
+            else:
+                split_dir = ""
+            destination = str(PurePath(path.parent, split_dir, "labels", annot_txt_file))
+            Path(
+                path.parent,
+                split_dir,
+                "labels",
+            ).mkdir(parents=True, exist_ok=True)
+
+            # If segmentation = false then output bounding boxes
+            if segmentation == False:
+
+                df_single_img_annots.to_csv(
+                    destination,
+                    index=False,
+                    header=False,
+                    sep=" ",
+                    float_format="%.4f",
+                    columns=[
+                        "cat_id",
+                        "center_x_scaled",
+                        "center_y_scaled",
+                        "width_scaled",
+                        "height_scaled",
+                    ],
+                )
+
+            # If segmentation = true then output the segmentation mask
+            else:
+                # Create one file for image
+                with open(destination, "w") as file:
+                    # Create one row per row in the data frame
+                    for i in range(0, df_single_img_annots.shape[0]):
+                        row = df_single_img_annots.iloc[i].cat_id + " "
+                        segmentation_array = df_single_img_annots.iloc[
+                            i
+                        ].ann_segmentation[0]
+
+                        # Iterate through every value of the segmentation array
+                        # To normalize the coordinates from 0-1
+                        for index, l in enumerate(segmentation_array):
+                            # The first number in the array is the x value so divide by the width
+                            if index % 2 == 0:
+                                row += (
+                                    str(
+                                        round(
+                                            segmentation_array[index]
+                                            / df_single_img_annots.iloc[i].img_width,
+                                            5,
+                                        )
+                                    )
+                                    + " "
+                                )
+                            else:
+                                # The first number in the array is the x value so divide by the height
+                                row += (
+                                    str(
+                                        round(
+                                            segmentation_array[index]
+                                            / df_single_img_annots.iloc[i].img_height,
+                                            5,
+                                        )
+                                    )
+                                    + " "
+                                )
+
+                        file.write(row + "\n")
+
+            output_file_paths.append(destination)
+
+            if copy_images:
+                source_image_path = str(
+                    Path(
+                        ds.path_to_annotations,
+                        df_single_img_annots.iloc[0].img_folder,
+                        df_single_img_annots.iloc[0].img_filename,
+                    )
+                )
+
+                current_file = Path(source_image_path)
+                assert (
+                    current_file.is_file
+                ), f"File does not exist: {source_image_path}. Check img_folder column values."
+                Path(path.parent, split_dir, "images").mkdir(
+                    parents=True, exist_ok=True
+                )
+                shutil.copy(
+                    str(source_image_path),
+                    str(PurePath(path.parent, split_dir, "images", img_filename)),
+                )
+
+        # Create YAML file
+        if yaml_file:
+            # Make a set with all of the different values of the split column
+            splits = set(yolo_dataset.split)
+            # Build a dict with all of the values that will go into the YAML file
+            dict_file = {}
+            dict_file["path"] = path_dict["root_path_from_yolo_dir"]
+
+            # If train is one of the splits, append train to path
+            if use_splits and "train" in splits:
+                dict_file["train"] = str(PurePath(path.parent, "train", "images"))
+            else:
+                dict_file["train"] = path_dict["image_path"]
+
+            # If val is one of the splits, append val to path
+            if use_splits and "val" in splits:
+                dict_file["val"] = str(PurePath(path.parent, "val", "images"))
+            else:
+                # If there is no val split, use the train split as the val split
+                dict_file["val"] = dict_file["train"]
+
+            # If test is one of the splits, make a test param and add test to the path
+            if use_splits and "test" in splits:
+                dict_file["test"] = str(PurePath(path.parent, "test", 'images'))
+
+            dict_file["nc"] = ds.analyze.num_classes
+            dict_file["names"] = ds.analyze.classes
+
+            # Save the yamlfile
+            with open(path_dict["yaml_path"], "w") as file:
+                documents = yaml.dump(dict_file, file)
+                output_file_paths = [path_dict["yaml_path"]] + output_file_paths
+
+        return output_file_paths
 
     def ExportToCoco(self, output_path=None, cat_id_index=None):
         """
