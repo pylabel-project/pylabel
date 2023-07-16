fbea4f581a2fd8e45baff0a13db8d5c4b3f18ebf
Alex Heaton <aheaton@gmail.com>
2023-07-16 19:05:01 +0900

    Bump v# to 52

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="pylabel",
     packages=["pylabel"],
-    version="0.1.51",
+    version="0.1.52",
     description="Transform, analyze, and visualize computer vision annotations.",
     long_description=long_description,
     long_description_content_type="text/markdown",
